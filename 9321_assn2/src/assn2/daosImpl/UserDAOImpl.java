0fac0a6fa5aceb8b1153e4c34dba270a92363b30
manni <zhuangdeyouxiang@gmail.com>
2012-05-05 01:44:05 +1000

    change userdao junit test, add exception class file to directory

--- 9321_assn2/src/assn2/daosImpl/UserDAOImpl.java
+++ 9321_assn2/src/assn2/daosImpl/UserDAOImpl.java
@@ -1,14 +1,171 @@
-/**
- * 
- */
 package assn2.daosImpl;
-
 import assn2.daos.UserDAO;
+import assn2.database.*;
+import assn2.exceptions.*;
+import assn2.beans.UserBean;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 
 /**
- * @author Kaihang
- *
+ * The Data Access Object for users.
  */
 public class UserDAOImpl implements UserDAO {
+	
+	private DBConnectionFactory services;
+	
+	public UserDAOImpl() throws ServiceLocatorException {
+		services = new DBConnectionFactory();
+	}
 
+	public UserDAOImpl(DBConnectionFactory services) {
+		this.services = services;
+	}
+
+	/**
+	 * @see com.enterprise.dao.UserDAO#findByLoginDetails(java.lang.String, java.lang.String)
+	 */
+	public UserBean findByLoginDetails(String username, String password) throws DataAccessException {
+		Connection con = null;
+		try {
+			con = services.createConnection();
+			PreparedStatement stmt = con.prepareStatement("select * from users where username = ? and password = ?");
+			stmt.setString(1, username);
+			stmt.setString(2, password);
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				UserBean user = createUserBean(rs);
+				stmt.close(); 
+				return user;
+			}
+		} catch (ServiceLocatorException e) {
+			throw new DataAccessException("Unable to retrieve connection; " + e.getMessage(), e);
+		} catch (SQLException e) {
+			throw new DataAccessException("Unable to execute query; " + e.getMessage(), e);
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
+	
+	//insert a new record in user table
+	public void InsertUserBean(UserBean bean) {
+		Connection con = null;
+		   try {
+			 //get the connection 
+			 con = services.createConnection();
+		 	 //create the prepared statement
+//			 Statement st = con.createStatement();
+//			 st.execute("select id from tbl_contacts");
+//			 
+			 PreparedStatement stmt = con.prepareStatement("Insert Into user (userid, fname, lname, security_level, email, username,password,address) values (?, ?, ?, ?, ?, ?,?,?)");
+		     stmt.setInt(1, bean.getUserid());
+		     stmt.setString(2, bean.getFname());
+		     stmt.setString(3, bean.getLname());
+		     stmt.setString(4, bean.getSecurity_level());
+		     stmt.setString(5, bean.getEmail());
+		     stmt.setString(6, bean.getUsername());
+		     stmt.setString(7, bean.getPassword());
+		     stmt.setString(8, bean.getAddress());
+		     
+		     //execute the update
+		     int n = stmt.executeUpdate();
+		     if (n != 1)//remember to catch the exceptions
+		 	   throw new DataAccessException("Did not insert one row into database");
+		   } catch (ServiceLocatorException e) {
+		       throw new DataAccessException("Unable to retrieve connection; " + e.getMessage(), e);
+		   } catch (SQLException e) {
+		       throw new DataAccessException("Unable to execute query; " + e.getMessage(), e);
+		   } finally {
+		      if (con != null) {
+		         try {
+		           con.close();//and close the connections etc
+		   		 } catch (SQLException e1) {  //if not close properly
+		           e1.printStackTrace();
+		         }
+		      }
+		   }
+	}
+	
+	public UserBean getUserBean(int id) {
+		Connection con = null;
+		UserBean r = null;
+		try {
+			 //get the connection 
+			 con = services.createConnection();		 
+			 PreparedStatement stmt = con.prepareStatement("select * from user where userid=(?)");
+		     stmt.setInt(1, id);
+		     ResultSet rs = stmt.executeQuery();
+			 if (rs == null)//remember to catch the exceptions
+			 	  throw new DataAccessException("cannot find any record owned by that id");
+			 while(rs.next()){
+				r = createUserBean(rs);
+			 }
+		   } catch (ServiceLocatorException e) {
+		       throw new DataAccessException("Unable to retrieve connection; " + e.getMessage(), e);
+		   } catch (SQLException e) {
+		       throw new DataAccessException("Unable to execute query; " + e.getMessage(), e);
+		   } finally {
+		      if (con != null) {
+		         try {
+		           con.close();//and close the connections etc
+		   		 } catch (SQLException e1) {  //if not close properly
+		           e1.printStackTrace();
+		         }
+		   }	      
+		}
+		return r; //the last
+	}
+	
+	public void deleteUserBean(int id) {
+		Connection con = null;
+		
+		try {
+			 //get the connection 
+			 con = services.createConnection();		 
+			 PreparedStatement stmt = con.prepareStatement("delete from user where userid=(?)");
+		     stmt.setInt(1, id);
+		     int status = stmt.executeUpdate();
+			 if (status != 1)//remember to catch the exceptions
+			 	  throw new DataAccessException("cannot delete any record owned by that id");
+			 
+		   } catch (ServiceLocatorException e) {
+		       throw new DataAccessException("Unable to retrieve connection; " + e.getMessage(), e);
+		   } catch (SQLException e) {
+		       throw new DataAccessException("Unable to execute query; " + e.getMessage(), e);
+		   } finally {
+		      if (con != null) {
+		         try {
+		           con.close();//and close the connections etc
+		   		 } catch (SQLException e1) {  //if not close properly
+		           e1.printStackTrace();
+		         }
+		   }	      
+		}
+		//return nothing !!
+	}
+	
+	//helper 
+	private UserBean createUserBean(ResultSet rs) throws SQLException {
+		UserBean user = new UserBean();
+		user.setAddress(rs.getString("address"));
+		user.setEmail(rs.getString("email"));
+		user.setFname(rs.getString("fname"));
+		user.setLname(rs.getString("lname"));
+		user.setPassword(rs.getString("password")); //column
+		user.setSecurity_level(rs.getString("security_level"));
+		user.setUserid(rs.getInt("userid"));
+		user.setUsername(rs.getString("username"));
+		return user;
+	}
 }
---
+++ 9321_assn2/src/assn2/daosImpl/UserDAOImplTest.java
@@ -0,0 +1,93 @@
+package assn2.daosImpl;
+/**
+ * Modified on 4th May
+ */
+import static org.junit.Assert.*;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.Reference;
+import javax.naming.StringRefAddr;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import assn2.beans.UserBean;
+import assn2.database.DBConnectionFactory;
+
+
+public class UserDAOImplTest extends TestCase {
+
+	private UserDAOImpl userdaoimpl;
+
+	public UserDAOImplTest(String arg0) {
+		super(arg0);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty(Context.INITIAL_CONTEXT_FACTORY,
+				"org.apache.naming.java.javaURLContextFactory");
+		System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");
+		InitialContext ic = new InitialContext();
+		ic.createSubcontext("java:");
+		ic.createSubcontext("java:/comp");
+		ic.createSubcontext("java:/comp/env");
+		ic.createSubcontext("java:/comp/env/jdbc");
+
+		// Construct BasicDataSource reference
+		Reference ref = new Reference("javax.sql.DataSource",
+				"org.apache.commons.dbcp.BasicDataSourceFactory", null);
+		ref.add(new StringRefAddr("driverClassName", "com.mysql.jdbc.Driver"));
+		ref.add(new StringRefAddr("url", "jdbc:mysql://127.0.0.1:33066/mydb"));
+		ref.add(new StringRefAddr("username", "root"));
+		ref.add(new StringRefAddr("password", ""));
+		ic.bind("java:/comp/env/jdbc/mydb", ref);
+		//initialize
+		userdaoimpl = new UserDAOImpl(new DBConnectionFactory());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.setProperty(Context.INITIAL_CONTEXT_FACTORY,
+				"org.apache.naming.java.javaURLContextFactory");
+		System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");
+
+		InitialContext ic = new InitialContext();
+		ic.unbind("java:/comp/env/jdbc/mydb");
+		ic.destroySubcontext("java:/comp/env/jdbc");
+		ic.destroySubcontext("java:/comp/env");
+		ic.destroySubcontext("java:/comp");
+		ic.destroySubcontext("java:");
+	}
+	
+	@Test
+	public void testGet() {
+		UserBean o1 = userdaoimpl.getUserBean(1); //to be checked
+		UserBean o2 = new UserBean(1,"steven","jobs","owner","sj@sj.com","sj","sj","us");
+		assertEquals(o1,o2);
+	}
+	
+	@Test
+	public void testAddandDelete() {
+		//first try delete the 10th record
+		try{
+			userdaoimpl.deleteUserBean(10);
+		}catch(Exception e){//add normal user
+			e.printStackTrace();
+		}
+		UserBean n = new UserBean(10,"min","han","user","mh@mh.com","mh","mh","oz");
+		userdaoimpl.InsertUserBean(n);
+		UserBean o = userdaoimpl.getUserBean(10);
+		assertEquals(n,o);
+		userdaoimpl.deleteUserBean(10);//cancel out the testing data
+	}
+	
+	public void testFindByLoginDetails() {
+		fail("Not yet implemented");
+	}
+
+}
